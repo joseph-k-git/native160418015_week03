764590b90d4029ae3c13b02bb41d2031ae2f6ab0
Joseph Kristiano <o.joseph.k@gmail.com>
2021-09-08 12:25:18 +0700

    Week 03 - Dialog option

---
+++ app/src/main/java/com/joseph18/ifubaya/adv160418015week2/OptionFragment.kt
@@ -0,0 +1,17 @@
+package com.joseph18.ifubaya.adv160418015week2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class OptionFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_option, container, false)
+    }
+}
